a1d3cbff9931317c3e61c5288b6dc0900a02e8a1
Yimy <zepedayimy46@gmail.com>
2022-06-16 01:47:30 +0200

    sasas

--- src/main/java/com/desarrollo/ServicePacksApplication.java
+++ src/main/java/com/desarrollo/ServicePacksApplication.java
@@ -2,8 +2,11 @@ package com.desarrollo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
 
 @SpringBootApplication
+@EnableEurekaClient
 public class ServicePacksApplication {
 
 	public static void main(String[] args) {
